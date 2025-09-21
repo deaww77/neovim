ba7e4933692cfa4f940edd51c19eed242bc3fe16
deaww77 <deawffe0@gmail.com>
2025-09-21 11:57:40 +0700

    add keyblind splite window

--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -1,13 +1,20 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>cd", vim.cmd.Ex)
--- ไป buffer ถัดไป/ก่อนหน้า
-vim.keymap.set('n', '<Tab>', '<Cmd>BufferNext<CR>')
-vim.keymap.set('n', '<S-Tab>', '<Cmd>BufferPrevious<CR>')
 
--- ปิด buffer ปัจจุบัน
-vim.keymap.set('n', '<leader>c', '<Cmd>BufferClose<CR>')
 
--- ไป buffer ตามหมายเลข (1–9)
-vim.keymap.set('n', '<leader>1', '<Cmd>BufferGoto 1<CR>')
-vim.keymap.set('n', '<leader>2', '<Cmd>BufferGoto 2<CR>')
+vim.keymap.set('n', '<leader>c', '<Cmd>BufferClose<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>n', '<Cmd>BufferNext<CR>',  { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>p', '<Cmd>BufferPrevious<CR>',  { noremap = true, silent = true })
 
+vim.keymap.set('n', '<leader>-', ':split<CR>', { silent = true })
+-- split แนวตั้ง
+vim.keymap.set('n', '<leader>=', ':vsplit<CR>', { silent = true })
+
+-- ย้ายไปยัง split ข้างๆ ด้วย leader + h/j/k/l
+vim.keymap.set('n', '<leader>h', '<C-w>h')
+vim.keymap.set('n', '<leader>j', '<C-w>j')
+vim.keymap.set('n', '<leader>k', '<C-w>k')
+vim.keymap.set('n', '<leader>l', '<C-w>l')
+
+-- ปิด split ปัจจุบัน
+vim.keymap.set('n', '<leader>q', '<C-w>c')
